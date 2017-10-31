fdc635587006cbfd1e6fe6ad309e3e70216e7503
MeePwn <maybewaityou@gmail.com>
2017-11-01 06:14:14 +0800

    first commit

--- src/business-module/zxc/container/ZxcContainer.ts
+++ src/business-module/zxc/container/ZxcContainer.ts
@@ -8,7 +8,7 @@
 import { inject, ViewModel } from 'mario-architecture-components';
 import { bind } from 'mario-ducks';
 import { mapStateToProps } from '../ducks/selector/index';
-import { ZxcViewModel, IViewModelProps } from '../view-model/index';
-import { ZxcView, IViewProps } from '../view/index';
+import { IViewModelProps, ZxcViewModel } from '../view-model/index';
+import { IViewProps, ZxcView } from '../view/index';
 
 export default bind(mapStateToProps)(inject<IViewProps, IViewModelProps, ViewModel<IViewModelProps>>(ZxcView, ZxcViewModel));
